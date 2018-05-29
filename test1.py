9c82762dce4d4685793438d676760823484829c8
宇宙総姐夫 <xuechong87@gmail.com>
2018-05-29 16:09:57 +0800

    打印title

--- test1.py
+++ test1.py
@@ -1,13 +1,20 @@
+#coding:utf-8
 
 import time
 from selenium import webdriver
 
-driver = webdriver.Chrome('C:\Program Files (x86)\Google\Chrome\Application\chromedriver')  # Optional argument, if not specified will search path.
+driver = webdriver.Chrome('C:\Program Files (x86)\Google\Chrome\Application\chromedriver')  # 写地址
 driver.get('https://www.eastlending.com/')
-titleEle = driver.find_element_by_tag_name('base')
+baseEle = driver.find_element_by_tag_name('base')
 
-print(titleEle.get_attribute('href'))
-time.sleep(5)
+print('base href is :' + baseEle.get_attribute('href'))
+
+titleEle = driver.find_element_by_tag_name('title')
+titleStr = titleEle.parent.title
+
+print('title is :' + titleStr)
+
+# time.sleep(1)
 driver.quit()
 print('press any key')
 input()
